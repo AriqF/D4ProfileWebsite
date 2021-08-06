1a6945034f237f453315aca809e6f3197c00190e
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-08-06 10:46:59 +0700

    create index page

---
+++ webprofile/resources/views/index.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Welcome</title>
+        @extends('layouts.head')
+        <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css"> 
+    </head>
+        <body style="background-color: #F2F2F2;">
+        <!--extends navbar-->
+        @extends('layouts.indexLayout')
+        @section('mainContent')
+        
+        <header class="pagehead" style="background-image: url('{{asset('img/ut.jpg')}}');">
+            <div class="container h-100">
+                <div class="d-flex bd-highlight h-100 align-items-center">
+                    <div class="p-2 flex-fill bd-highlight text-left">
+                        <h1 id="page-title">Halaman Profil D4 Manajemen Informatika</h1>
+                        <p class="sub-title">Kami adalah mahasiswa informatika hu ha hu haaa</p>
+                        <a href="#info-section" class="btn-explore js-scroll-trigger" role="button" id="btn-exp">Jelajahi</a>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+        <section class="page-section" id="info-section">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-12 col-md-12 mb-4">
+                        <div class="px-lg-5">
+                            <h2 class="text-center mt-0 section-title">Informasi Berita Terbaru</h2>
+                            <div class="row">  
+
+                                <div class="col-xl-6 col-lg-6 col-md-6 mb-4">
+                                    <div class="card">
+                                        <img class="card-img-top" src="{{ asset('img/unesa.jpg') }}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <div class="p-2 bd-highlight">
+                                            <h5 class="font-weight-bold card-news-title">Halo 1</h5>
+                                            <p class="news-date-update"><i class="far fa-calendar-alt"></i> 08 Agustus 2021</p>
+                                            </div>
+                                            <div class="p-2 bd-highlight">
+                                                <a href="#" class="card-read-more">Lanjut Baca <i class="fas fa-angle-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-xl-6 col-lg-6 col-md-6 mb-4">
+                                    <div class="card">
+                                        <img class="card-img-top" src="{{ asset('img/5.jpg') }}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <div class="p-2 bd-highlight">
+                                            <h5 class="font-weight-bold card-news-title">Halo 1</h5>
+                                            <p class="news-date-update"><i class="far fa-calendar-alt"></i> 08 Agustus 2021</p>
+                                            </div>
+                                            <div class="p-2 bd-highlight ">
+                                                <a href="#" class="card-read-more">Lanjut Baca <i class="fas fa-angle-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 mb-4">
+                                    <div class="card">
+                                        <img class="card-img-top" src="{{ asset('img/unesa.jpg') }}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <div class="p-2 bd-highlight">
+                                            <h5 class="font-weight-bold card-news-title">Halo 1</h5>
+                                            <p class="news-date-update"><i class="far fa-calendar-alt"></i> 08 Agustus 2021</p>
+                                            </div>
+                                            <div class="p-2 bd-highlight">
+                                                <a href="#" class="card-read-more">Lanjut Baca <i class="fas fa-angle-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 mb-4">
+                                    <div class="card">
+                                        <img class="card-img-top" src="{{ asset('img/ut.jpg') }}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <div class="p-2 bd-highlight">
+                                            <h5 class="font-weight-bold card-news-title">Halo 1</h5>
+                                            <p class="news-date-update"><i class="far fa-calendar-alt"></i> 08 Agustus 2021</p>
+                                            </div>
+                                            <div class="p-2 bd-highlight">
+                                                <a href="#" class="card-read-more">Lanjut Baca <i class="fas fa-angle-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div> <!--row div-->
+                        </div> <!--px-lg-5 div-->
+                    </div>
+                    <div class="col-xl-4 col-lg-12 col-md-12 mb-4">
+                        <div class="px-lg-5">
+                            <h2 class="text-center mt-0 section-title">Informasi Lawas</h2>
+                            <div class="list-group list-group-mine">
+                                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start hov">
+                                    <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Heading 1</h5>
+                                    <small>5 days ago</small>
+                                    </div>
+                                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    <small>Lorem Ipsum.</small>
+                                </a>
+                                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Heading 2</h5>
+                                    <small>10 days ago</small>
+                                    </div>
+                                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                                    <small>Lorem Ipsum.</small>
+                                </a>
+                                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Heading 3</h5>
+                                    <small>12 days ago</small>
+                                    </div>
+                                    <p class="mb-1">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                                    <small >Lorem Ipsum..</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div> <!--container-fluid-->
+        </section>
+        @endsection   
---
+++ webprofile/resources/views/layouts/indexLayout.blade.php
@@ -0,0 +1,119 @@
+    <!--navbar here-->
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background-color: #002366; font-family: 'Maven Pro', sans-serif;">
+        <a class="navbar-brand" href="#">IfMatics</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Tentang Kami
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Visi Misi</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Struktur Organisasi</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Akreditasi</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Aktifitas Sosial</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Prestasi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fasilitas</a>
+                </li>
+                
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+                @if (Route::has('login'))
+                    @auth
+                        <a class="userLinks" href="{{ url('/dashboard') }}" type="submit">#username</a>
+                    @else
+                            <a class="btn-login" type="submit" href="{{ route('login') }}">Login</a>
+                        @if (Route::has('register'))
+                            <a class="btn-register" type="submit" href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                @endif
+
+            </div>
+        </div>
+    </nav>
+    @yield('mainContent')
+
+    <!--footer-->
+
+    <section class="page-section footer">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-lg-4 text-left">
+                <ul style="list-style: none;">
+                        <li ><h2>//Change Later</h2></li>
+                        <li >7-chōme-3-1 Hongō, Bunkyo City, Tokyo 113-8654, Japan</li>
+                        <li >P. +93732374</li>
+                        <li >M. if-matics-project@mailer.com</li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 text-left">
+                <ul style="list-style: none;">
+                        <li ><h2>//Change Later</h2></li>
+                        <li >7-chōme-3-1 Hongō, Bunkyo City, Tokyo 113-8654, Japan</li>
+                        <li >P. +93732374</li>
+                        <li >M. if-matics-project@mailer.com</li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 text-left">
+                    <ul style="list-style: none;">
+                        <li ><h2>Hubungi Kami</h2></li>
+                        <li >7-chōme-3-1 Hongō, Bunkyo City, Tokyo 113-8654, Japan</li>
+                        <li >P. +93732374</li>
+                        <li >M. if-matics-project@mailer.com</li>
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <d  iv class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+
+                </div>
+                <div class="col-lg-4 mr-auto text-center">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer-->
+    <footer class="py-3 bottom-footer" style="color: white; border-top: 0.5px solid #5C5557">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 small text-center footer-bottom-text">
+                    IfMatics University
+                </div>
+                <div class="col-4 small text-center footer-bottom-text">
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                    Project IfMatics
+                </div>
+                <div class="col-4 small text-center footer-bottom-text">
+                    Department of IfMatics 
+                </div>
+            </div>
+
+
+        </div>
+    </footer>
+</body>
+<!--anime js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
