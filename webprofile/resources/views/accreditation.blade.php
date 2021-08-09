e2d07135cfad86c60256394b3474dfcd5310e596
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-08-09 19:46:00 +0700

    Create accreditation.blade.php

---
+++ webprofile/resources/views/accreditation.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Welcome</title>
+        @extends('layouts.head')
+        <link rel="stylesheet" href="{{ asset('css/index.css') }}" type="text/css"> 
+        
+    </head>
+        <body style="background-color: #F2F2F2;">
+        <!--extends navbar-->
+        @extends('layouts.indexLayout')
+        @section('mainContent')
+        <header class="pagehead" id="bg-visionmission">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                        <div class="col-12 text-center">
+                            <h1 class="page-title">Akreditasi</h1>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </header>
+        <section class="page-section info-section">
+            <div class="container-fluid">
+                <div class="container">
+                   <h2 class="nvm-header">//insert image later</h2>
+                    <!--insert img--> 
+                </div>
+                
+            </div>
+        </section>
+
+
+
+        @endsection   
