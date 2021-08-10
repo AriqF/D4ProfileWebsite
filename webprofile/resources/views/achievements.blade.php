79174f9049cc766ce6795fe875ff7f7cc009f9b1
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-08-10 20:32:54 +0700

    add modal

--- webprofile/resources/views/achievements.blade.php
+++ webprofile/resources/views/achievements.blade.php
@@ -32,7 +32,35 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td class="table-awards-title">Contrary to popular belief, Lorem Ipsum is not simply random text 2021</td>
+                                <td class="table-awards-title">
+                                   <a class="modal-link-title" data-toggle="modal" data-target="#award-detail">Contrary to popular belief, Lorem Ipsum is not simply random text 2021</a> 
+                                   <!-- Modal -->
+                                    <div class="modal fade" id="award-detail" tabindex="-1" role="dialog" aria-labelledby="award-detail-label" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="award-detail-label">Award Title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="container">
+                                                <img src="{{ asset('img/5.jpg') }}" class="img-fluid card-img-top" alt="Image Karya" style="height: fit-content; width:fit-content">
+                                            </div>
+                                            <div class="container" style="margin-top: 20px;">
+                                                <p class="modal-lead">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia</p>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </td>
                                 <td class="table-awards-category">Random Text</td>
                                 <td class="table-awards-year">2021</td>
                             </tr>
